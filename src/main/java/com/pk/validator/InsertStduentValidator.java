1579b57bf11655f210a83abd6ca87b8dd6210059
pankajkharwade007 <pankaj.kharwade.pk@gmail.com>
2019-08-02 16:52:16 +0530

    InsertStudentValidator java file is added

---
+++ src/main/java/com/pk/validator/InsertStduentValidator.java
@@ -0,0 +1,20 @@
+package com.pk.validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class InsertStduentValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
