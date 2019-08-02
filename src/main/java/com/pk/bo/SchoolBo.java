270549459e5ed02a21525e7b2bc6c4eb1082b09c
pankajkharwade007 <pankaj.kharwade.pk@gmail.com>
2019-08-02 17:14:12 +0530

    Changes in SchoolBo file

--- src/main/java/com/pk/bo/SchoolBo.java
+++ src/main/java/com/pk/bo/SchoolBo.java
@@ -1,5 +1,64 @@
 package com.pk.bo;
 
 public class SchoolBo {
-
+	
+	private int srNo;
+	private String name;
+	private int standard;
+	private int rollNo;
+	private int mark1;
+	private int marks2;
+	private int marks3;
+	private String result;
+	public int getSrNo() {
+		return srNo;
+	}
+	public void setSrNo(int srNo) {
+		this.srNo = srNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStandard() {
+		return standard;
+	}
+	public void setStandard(int standard) {
+		this.standard = standard;
+	}
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public int getMark1() {
+		return mark1;
+	}
+	public void setMark1(int mark1) {
+		this.mark1 = mark1;
+	}
+	public int getMarks2() {
+		return marks2;
+	}
+	public void setMarks2(int marks2) {
+		this.marks2 = marks2;
+	}
+	public int getMarks3() {
+		return marks3;
+	}
+	public void setMarks3(int marks3) {
+		this.marks3 = marks3;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
+	
 }
